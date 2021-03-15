132131034b8d2ecec1820eb297d65fcc07732653
wayanlw <wayanlw@gmail>
2021-03-15 11:10:22 +1100

    comments

--- Decode the Morse code.py
+++ Decode the Morse code.py
@@ -2,7 +2,7 @@
 
 def decodeMorse(morse_code):
     finalstring = ''
-    chars =''
+    chars = ''
     for splitWords in morse_code.strip().split('   '):
         for splitchars in splitWords.split(' '):
             chars = chars + MORSE_CODE[splitchars]
@@ -17,13 +17,18 @@ def decodeMorse(morse_code):
 # --------------------------------- methond 2 -------------------------------- #
 
 def decodeMorse(morse_code):
-    sent=[]
-    word =''
+    '''
+    Decoding using an array
+    '''
+    sent = []
+    word = ''
 
     for splitWords in morse_code.strip().split('   '):
+                                                 #splitting the morse code in to words by splitting using 3 spaces
         for splitchars in splitWords.split(' '):
-            word = word + MORSE_CODE[splitchars]
-        sent.append(word)
-        word = ''
+                                                 ## splitting the most code in to characters by splitting using 1 space
+            word = word + MORSE_CODE[splitchars] #adding the letters to the string
+        sent.append(word)                        # adding to a string thinking of using ''.join() at the end
+        word = ''                                # have to make a blank string. Else the previous words will be present.
 
     return ' '.join(sent)
