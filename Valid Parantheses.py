9f9807a19adfa37c9ac12c60b9eb9ce9ebe49492
wayanlw <wayanlw@gmail>
2021-03-16 18:39:35 +1100

    New Files

---
+++ Valid Parantheses.py
@@ -0,0 +1,35 @@
+"""
+Collect|
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+"""
+# -------------------------------- my solution ------------------------------- #
+
+def valid_parentheses(string):
+    paran = []
+    for i in string:
+        if i =='(' or i== ')':
+            paran.append(i)
+    finString = ''.join(paran)
+
+    while '()' in finString:
+        finString = finString.replace('()','')
+
+    return not finString
+
+print (valid_parentheses('p(tdms)qgz(tc(rx)nk)v()f'))
+
+
+# --------------------------------- option 2 --------------------------------- #
+def valid_parentheses(string):
+    cnt = 0
+    for char in string:
+        if char == '(': cnt += 1
+        if char == ')': cnt -= 1
+        if cnt < 0: return False
+    return True if cnt == 0 else False
